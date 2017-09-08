4b9cafe493e56e489ac777113263cf89892067b5
lsp <kokoy@163.com>
2017-09-08 15:43:31 +0800

    no

--- src/cn/edu/cau/netcenter/Main.java
+++ src/cn/edu/cau/netcenter/Main.java
@@ -9,7 +9,7 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        // asdf
+        //
         System.out.println("Hello World! dsfsfsddfsdf");/**/
     }
 }
